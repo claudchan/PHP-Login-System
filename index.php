d46ea279960479762ca143afe93a0edf8d87de8d
Chan Chee Chiew <cheechiew.chan@mirumagency.com>
2017-11-15 15:06:47 +0800

    Hello World

--- index.php
+++ index.php
@@ -1,2 +1,45 @@
-<?php echo "Hello World"; ?>
+<!DOCTYPE html>
+<html lang="en">
+	<head>
 
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+		<title>Page Title</title>
+
+		<base href="/PHP-Login-System/" />
+
+		<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css" />
+
+	</head>
+
+	<body>
+
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-8 col-sm-offset-2 col-md-6 col-sm-offset-3">
+					<h1>Login</h1>
+					<form>
+						<div class="form-group">
+							<label for="inputEmail">Email</label>
+							<input type="email" class="form-control" id="inputEmail" name="inputEmail" placeholder="Email" required>
+						</div>
+						<div class="form-group">
+							<label for="inputPassword">Password</label>
+							<input type="password" class="form-control" id="inputPassword" name="inputPassword" placeholder="Password" required>
+						</div>
+						<button type="submit" class="btn btn-default">Submit</button>
+					</form>
+				</div>
+			</div>
+		</div>
+
+		<!-- jQuery is required -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+		<!-- Bootstrap Framework JS -->
+		<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+	</body>
+</html>
